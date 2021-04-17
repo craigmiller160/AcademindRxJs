dfb1d0b0acfa10b2cfef815d74fac08b27d02818
Craig <craigmiller160@gmail.com>
2021-04-17 09:15:59 -0400

    Modified example to use RxJs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const button = document.querySelector('button');
-button.addEventListener('click', (event) => {
-    console.log(event);
-});
+// button.addEventListener('click', (event) => {
+//     console.log(event);
+// });
+
+Rx.Observable.fromEvent(button, 'click')
+    .throttleTime(1000)
+    .subscribe(
+        (event) => console.log(event)
+    );
