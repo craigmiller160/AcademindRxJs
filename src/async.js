35e83a7e66eea8c9f0679ce343a5c418d592568e
Craig Miller <craigmiller160@gmail.com>
2021-04-17 13:18:54 -0400

    Fixed stuff

--- src/async.js
+++ src/async.js
@@ -1,7 +1,33 @@
-const button = document.querySelector('#async-btn');
-const div = document.querySelector('#async-div');
+const asyncBtn = document.querySelector('#async-btn');
+const asyncDiv = document.querySelector('#async-div');
 
 const setText = (text) => {
-    div.textContent = text;
+    asyncDiv.textContent = text;
 };
 
+const checkAuth = () =>
+    Rx.Observable.create(observer => {
+        setText('Checking Auth...');
+        setTimeout(() => {
+            observer.next(true);
+        }, 2000);
+    });
+
+const fetchUser = () =>
+    Rx.Observable.create(observer => {
+        setText('Fetching User...');
+        setTimeout(() => {
+            observer.next({ name: 'Max' });
+        }, 2000);
+    });
+
+Rx.Observable.fromEvent(asyncBtn, 'click')
+    .switchMap(event => checkAuth())
+    .switchMap(isAuth => {
+        if (isAuth) {
+            return fetchUser();
+        }
+    })
+    .subscribe(user => {
+        setText(user.name);
+    });
